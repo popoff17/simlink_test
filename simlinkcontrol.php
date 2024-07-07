47561a90b0296d823a5b4494d50e3d9979a3e573
Николай Попов <coder17@web-axioma.ru>
2024-07-07 23:59:12 +0500

    1

--- simlinkcontrol.php
+++ simlinkcontrol.php
@@ -1,14 +1,31 @@
 <?php
-$target = $_SERVER['DOCUMENT_ROOT'] . "/core";
-$link = $_SERVER['DOCUMENT_ROOT'] . "/test";
 
-if (file_exists($link)) {
-    echo "Симлинк уже существует.";
-} else {
-    if (symlink($target, $link)) {
-        echo "Симлинк успешно создан.";
-    } else {
-        echo "Не удалось создать симлинк. Пожалуйста, проверьте права доступа.";
-    }
+$simlinksArr = [
+				"/core" => "/subdomain/core"
+			];
+
+
+
+foreach($simlinksArr as $target=>$name){
+	$target = $_SERVER['DOCUMENT_ROOT'].$target;
+	$name = $_SERVER['DOCUMENT_ROOT'].$name;
+	
+	if (file_exists($name)) {
+		var_dump(readlink($name));
+		die("x");
+	}
+	
+	
+	
+	break;
+	if (file_exists($name)) {
+		echo "Симлинк уже существует.";
+	} else {
+		if (symlink($target, $name)) {
+			echo "Симлинк успешно создан.";
+		} else {
+			echo "Не удалось создать симлинк. Пожалуйста, проверьте права доступа.";
+		}
+	}
 }
 ?>
